823f4cae0a056243f6e46b804d143ab6d487a8ba
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2023-09-15 14:20:50 +0900

    edit #4.0

--- #4.0/app.py
+++ #4.0/app.py
@@ -1,12 +1,27 @@
-from flask import Flask,render_template
+# app.py@#4.0
+"""
+# urlパラメタとフォームの取得
+
+urlパラメタa,bを掛け合わせてgcdを返すプログラム
+
+"""
+from flask import Flask,request
+from function import gcd
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
-    return render_template(
-
-    )
+    a = request.args.get("a")
+    b = request.args.get("b")
+    if a is None:
+        return "パラメータ'a'が足りません"
+    if b is None:
+        return "パラメータ'b'が足りません"
+    result = gcd(int(a),int(b))
+    return f"gcd -> {result}"
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0")
