dacf0b7ef6c96e7adf54d2101d2c809db70002a3
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2023-09-15 13:49:36 +0900

    change folder name

---
+++ #3.0/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = "tom"
+    age = 10
+    email = "tom.ipynb@gmail.com"
+    return render_template(
+        "card.html",
+        name=name,
+        age=age,
+        email=email
+    )
+
+
+if __name__=="__main__":
+    app.run(debug=True)
